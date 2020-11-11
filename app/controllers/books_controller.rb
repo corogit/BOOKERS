7b7afe8216546a65006be5f596eaa9590aba1223
corogit <n.ohta5192@gmail.com>
2020-11-11 08:39:35 +0000

    2nd commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,19 +10,34 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book)
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
-  
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def top
+  end
+
   private
   def book_params
-    params.require(:book).permit(:title, :category, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
