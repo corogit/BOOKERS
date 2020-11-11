867bb29158b3d3bd149f83c9c4d00f421d6f4215
corogit <n.ohta5192@gmail.com>
2020-11-11 12:48:45 +0000

    4th commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -13,24 +14,36 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    if book.save
+    redirect_to book_path(book), notice: 'Book was successfully created.'
+    else
+  
+    render :index
+    end
   end
+
 
   def edit
      @book = Book.find(params[:id])
   end
+  
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+    redirect_to book_path(book), notice: 'Book was successfully updated.'
+    else render :index
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+  
+    end
+    
+    
   end
 
   def top
---
+++ app/views/books/book.html.erb
@@ -0,0 +1,6 @@
+<%= form_for(@names, url: names_path) do |f| %>
+<% if @names.errors.any? %>
+<%= @names.errors.count %>件のエラー
+<% @names.errors.full_messages.each do |message| %>
+<%= message %>
+<% end %>
